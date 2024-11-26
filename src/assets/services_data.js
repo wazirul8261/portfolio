8286640d45738c03d15be31e9db8aafa5c414a54
wazirul8261 <hutasona@gmail.com>
2024-11-26 13:51:11 +0600

    complete the project

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,32 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Are you looking for a Top Quality Website Design that should attract to your visitors & customers? Then you have landed in the right place! My goal is to create a professional design that will beat your competitor's. I will build an Professional & Responsive React design based on the latest trends in the market, tools, and technology please contact."
     },
     {
         s_no:"02",
         s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"I am a passionate and experienced graphic designer with a proven track record of delivering top-notch designs that exceed my clients expectations. With a keen eye for detail and a deep understanding of design principles, I pride myself on crafting compelling visuals that leave a lasting impression.If you need service please contact"
     },
     {
         s_no:"03",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"I am an experienced and knowledgeable Marketing and Presentation Specialist with a portfolio of over 50 clients and 200+ presentation projects completed in the last 3 years. I am highly responsive, creative, and deadline-oriented. If you want to bring your ideas to life in your presentation,If you need service please contact let's call"
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"YouTube Promotion",
+        s_desc:"Are you tired of creating high-quality YouTube videos that don't get the reach and engagement they deserve? Are you looking for a way to boost your video's performance and reach the right audience?. I optimize your video and SEO in a professional way and video will reach targeted audience, If you need such type of service please contact."
     },
     {
         s_no:"05",
         s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"I am currently working as a Digital Marketing Consultant with McDonald's to run their campaign on Google Adwords and have multiple international clients for Social Media Marketing like Facebook ads campain, Instagram. Pinterest. Let's work together to achieve your digital marketing goals! ✨If you need such type of service please contact ."
     },
     {
         s_no:"06",
         s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:" I am a professional researcher, creative writer, and specialist in on-page SEO, having 5+ years of experience. By writing SEO-optimized articles, website content, and blog posts for you in no time. I aim to provide you quality research and content that will help high rank your website in google and other search engines.  please contact given below ."
     },
 ]
  
